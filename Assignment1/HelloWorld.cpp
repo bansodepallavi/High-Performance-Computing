0ebcc27f498adb579619418ca537c37581a6b2d9
bansodepallavi <67478199+bansodepallavi@users.noreply.github.com>
2021-09-06 10:22:36 +0530

    Create HelloWorld.cpp

---
+++ Assignment1/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <omp.h>
+#include <stdio.h>
+#include<stdlib.h>
+int main(int argc,char* argv[]) {
+
+	//Beginning of parallel region
+	#pragma omp parallel
+	{
+		printf("Hello 2019BTECS00203 ... from thread=%d\n",omp_get_thread_num());
+	}
+	//Ending of parallel region
+}
+
